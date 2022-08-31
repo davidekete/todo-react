fc54c575582bdf898b4c9013743d171656935d3b
David Ekete <eketedavid@gmail.com>
2022-08-31 09:27:24 +0100

    bug: patch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-/* eslint-disable */
-// eslint-disable
-import './App.css';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import Loading from './components/loading';
-import TodoList from './components/TodoList';
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Loading from "./components/loading";
+import TodoList from "./components/TodoList";
+
+import createMockServer from "./mock";
+createMockServer();
 
 function App() {
   const [todos, setTodos] = useState(null);
+  const [userTodo, setUserTodo] = useState("");
+
+  const fetchApiData = function (path) {
+    axios.get("/api/todos").then((result) => {
+      setTodos(result.data);
+    }).then(()=> console.log(todos) )
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const body = {
+      title: userTodo,
+    };
+
+    const options = { headers: { accept: "application/json" } };
+    axios.post("/api/todos/new", body, options).then((res) => {
+      fetchApiData();
+    });
+
+
+    setUserTodo("");
+  };
 
   useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/todos/1').then((result) => {
-      setTodos([result.data]);
-    });
+    fetchApiData();
   }, []);
 
   return (
-    <div className='app'>
-      <div className='todos-container'>
-        {todos ? <TodoList todos={todos} /> : <Loading />}
+    <div className="app">
+      <div className="todos-container">
+        {todos ? (
+          <TodoList
+            todos={todos}
+            handleSubmit={handleSubmit}
+            setUserTodo={setUserTodo}
+            userTodo={userTodo}
+            setTodos={setTodos}
+          />
+        ) : (
+          <Loading />
+        )}
       </div>
     </div>
   );
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,18 +1,79 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/react-in-jsx-scope */
-/* eslint-disable react/jsx-filename-extension */
-function TodoList({ todos }) {
+import axios from "axios";
+import { useState } from "react";
+
+function TodoList({ todos, setTodos, handleSubmit, setUserTodo, userTodo }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [currentItem, setCurrentItem] = useState({ title: "", id: "" });
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+
+    axios.delete(`api/todos/${id}`).then(() => console.log(todos));
+  };
+
+  const updateTodo = (title, id) => {
+    console.log(title, id, todos);
+
+    todos.map((todo) => {
+      if (todo.id === id) {
+        todo.title = title;
+      }
+    });
+
+    setTodos(todos);
+  };
+
+  const handleCheckState = (id) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
   return (
-    <div className='todos'>
-      <h1>I see you want a heading</h1>
-      <ul className='list-group'>
+    <div className="todos">
+      <h1>Todo List</h1>
+
+      <ul className="list-group">
         {todos.map((todo) => (
-          <li className='list-group-item d-flex justify-content-between align-items-center'>
-            {todo.title}
-            <input type='checkbox' checked={todo.completed} />
+          <li
+            key={todo.id}
+            className="list-group-item d-flex justify-content-between align-items-center"
+          >
+            <input
+              value={todo.title}
+              className="px-3 todo-text-input"
+              onChange={(e) => updateTodo(e.target.value, todo.id)}
+            />
+
+            <div className="btncontainer">
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => handleCheckState(todo.id)}
+              />
+
+              <button className="close" onClick={() => deleteTodo(todo.id)}>
+                &times;
+              </button>
+            </div>
           </li>
         ))}
       </ul>
+      <form onSubmit={handleSubmit} className="todo-form">
+        {" "}
+        <input
+          className="input-field"
+          value={userTodo}
+          onChange={(e) => setUserTodo(e.target.value)}
+          placeholder="Enter your todo"
+          type="text"
+        />
+        <button type="submit"> &#10003;</button>{" "}
+      </form>
     </div>
   );
 }
